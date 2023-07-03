f09b397e924a1c04ab26b498a26ead819625be3f
Jeronymo Dalapicolla <34402608+jdalapicolla@users.noreply.github.com>
2023-07-03 11:21:09 -0300

    Create GeneticDiversityDistance_steerei.R

---
+++ GeneticDiversityDistance/GeneticDiversityDistance_steerei.R
@@ -0,0 +1,6 @@
+#################### UNIVERSIDADE FEDERAL DA PARAÍBA ##########################
+################ DEPARTAMENTO DE SISTEMÁTICA E ECOLOGIA #######################
+######################     FILTERING SNPs TUTORIAL     ########################
+
+#By Jeronymo Dalapicolla, 2023: Guia de genômica de populações aplicada a mamíferos Neotropicais
+
